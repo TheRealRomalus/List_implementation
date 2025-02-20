8b2cbdc1e84c61569c2bd304bb45fa213cc761d1
TheRealRomalus <rohams88@outlook.com>
2025-02-20 13:37:50 -0500

    Arraylist is done all functions work as they are supposed to , need test cases and also the edge cases

--- ListEvaluation/src/MyArrayList.java
+++ ListEvaluation/src/MyArrayList.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -6,64 +7,144 @@ import java.util.ListIterator;
 public class MyArrayList<E> implements List<E> {
 
     private E[] array;
-    private int size;
+    private int numb_elements;
     private static final int Initila_Capacity = 10;
-    int yab;
 
     public MyArrayList() {
         this(Initila_Capacity);
-        size = 0;
+        numb_elements = 0;
 
     }
 
     public MyArrayList(int capacity) {
-        if (capacity <= 0) {
+        if (capacity <= 10) {
             array = (E[]) new Object[Initila_Capacity];
-            size = 0;
 
         } else {
             array = (E[]) new Object[capacity];
         }
+        numb_elements = 0;
     }
 
     public boolean add(E element) {
-        if (array.length == size) {
-            resize();
-        }
+        add(numb_elements, element);
 
-        return false;
+        return true;
 
     }
 
     public void add(int index, E element) {
 
+        if (index < 0 || index > numb_elements) {
+            throw new IndexOutOfBoundsException("Index out of bound add");
+        }
+
+        if (array[index] == null) {
+            array[index] = element;
+            numb_elements++;
+            resize();
+
+        } else {
+
+            add(index + 1, array[index]);
+            array[index] = element;
+
+        }
+
     }
 
+    // just make size == 0
     public void clear() {
+        array = (E[]) new Object[Initila_Capacity];
+        numb_elements = 0;
 
     }
 
     public E remove(int index) {
-        return null;
+
+        if (index < 0 || index >= numb_elements) {
+            throw new IndexOutOfBoundsException("Index out of bound in remove");
+        }
+
+        E temp = array[index];
+
+        for (int i = index; i < numb_elements; i++) {
+            array[i] = array[i + 1];
+        }
+        numb_elements--;
+        resize();
+
+        return temp;
 
     }
 
     public boolean remove(Object object) {
+
+        int index = 0;
+        while (array[index] != null) {
+            if (array[index].equals((E) object)) {
+                return (remove(index) != null);
+
+            }
+            index++;
+        }
+
         return false;
 
     }
 
     public int size() {
-        return size;
+        return numb_elements;
     }
 
     public String toString() {
-        return "";
+
+        String result = "[";
+
+        for (E i : array) {
+            if (i != null) {
+                result += i + ", ";
+            }
+
+        }
+
+        if (numb_elements != 0) {
+            result = result.substring(0, result.length() - 2);
+
+        }
+
+        return result + "]";
 
     }
 
     public void resize() {
 
+        if (array.length / 2 <= numb_elements) {
+
+            // double size
+            E[] tempArr = (E[]) new Object[array.length * 2];
+
+            for (int i = 0; i < numb_elements; i++) {
+                tempArr[i] = array[i];
+            }
+            array = tempArr;
+
+        } else if (array.length / 4 >= numb_elements) {
+
+            //
+            E[] tempArr = (E[]) new Object[array.length / 2];
+
+            for (int i = 0; i < numb_elements; i++) {
+                tempArr[i] = array[i];
+            }
+            array = tempArr;
+
+        }
+
+    }
+
+    public void lenght() {
+        System.out.println("array length; " + array.length);
     }
 
     @Override
@@ -169,7 +250,84 @@ public class MyArrayList<E> implements List<E> {
     }
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        MyArrayList<String> test = new MyArrayList<>();
+
+        test.lenght();
+        test.add("eoiw");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("hello");
+        test.lenght();
+        test.add("eoiw");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("hello");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("hello");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+        test.remove(0);
+        test.lenght();
+
+        test.lenght();
+        test.add("eoiw");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("hello");
+        test.lenght();
+        test.add("eoiw");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("hello");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("hello");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+        test.add("ppp");
+        test.lenght();
+
+        System.out.println(test);
+        System.out.println(test.size());
+
     }
 
 }
